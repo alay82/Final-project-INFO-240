46b728a14cf8b9241aba0ec39fa1dfd44ebbaaa7
Allison <a.lay@hotmail.com>
2019-08-22 20:30:09 -0500

    first commit

---
+++ includes/navigation menu.php
@@ -0,0 +1,10 @@
+<p class="copyright">&copy; <?php echo date("Y") ?> <nav class="primary-menu">
+   <ul>
+      <li><a href="homepage.php">Home</a></li>
+      <li><a href="about me.php">About Me</a></li>
+	  <li><a href="photos.php">Photos</a></li>
+	  <li><a href="paintings.php">Paintings</a></li>
+	  <li><a href="contact me.php">Contact me</a></li>
+   </ul>
+   </nav>
+   </p>
---
+++ includes/navigation-menu.php
@@ -0,0 +1,10 @@
+<p class="copyright">&copy; <?php echo date("Y") ?> <nav class="primary-menu">
+   <ul>
+      <li><a href="homepage.html">Home</a></li>
+      <li><a href="about me.html">About Me</a></li>
+	  <li><a href="photos.html">Photos</a></li>
+	  <li><a href="paintings.html">Paintings</a></li>
+	  <li><a href="contact me.html">Contact me</a></li>
+   </ul>
+   </nav>
+   </p>
